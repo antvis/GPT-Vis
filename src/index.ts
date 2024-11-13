4f564b0c528622e4c24c368959358e2358d4cf98
yunji <yunji.me@outlook.com>
2024-11-13 21:05:38 +0800

    chore: export version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './export';
+export { default as version } from './version';
 
 export { withChartCode, withDefaultChartCode } from './ChartCodeRender';
 export { default as GPTVis, type GPTVisProps } from './GPTVis';
