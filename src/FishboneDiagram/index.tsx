367d80573949a6eeef631cf78932a405f7be4850
yunji <yunji.me@outlook.com>
2024-12-13 18:39:59 +0800

    chore: useGraphConfig type

--- src/FishboneDiagram/index.tsx
+++ src/FishboneDiagram/index.tsx
@@ -1,4 +1,4 @@
-import type { GraphOptions } from '@ant-design/graphs';
+import type { FishboneOptions } from '@ant-design/graphs';
 import { Fishbone as ADCFishbone } from '@ant-design/graphs';
 import React, { useMemo } from 'react';
 import { useGraphConfig } from '../ConfigProvider/hooks';
@@ -7,7 +7,7 @@ import { visTreeData2GraphData } from '../utils/graph';
 
 export interface FishboneDiagramProps extends TreeGraphProps {}
 
-const defaultConfig: GraphOptions = {
+const defaultConfig: FishboneOptions = {
   autoFit: 'view',
   autoResize: true,
 };
@@ -17,7 +17,7 @@ const FishboneDiagram: React.FC<FishboneDiagramProps> = (props) => {
 
   const data = useMemo(() => visTreeData2GraphData(propsData), [propsData]);
 
-  const config = useGraphConfig<GraphOptions>('FishboneDiagram', defaultConfig, restProps);
+  const config = useGraphConfig<FishboneOptions>('FishboneDiagram', defaultConfig, restProps);
 
   return <ADCFishbone data={data} {...config} />;
 };
