dbb4986181d5dd4df8bb3ccf3aef7950a7e8ba30
duxinyue.dxy <duxinyue.dxy@antgroup.com>
2025-05-19 20:39:27 +0800

    feat: empty lines

--- bindings/dify-plugin-visualization/provider/antv_charts.py
+++ bindings/dify-plugin-visualization/provider/antv_charts.py
@@ -2,7 +2,6 @@ from typing import Any
 from dify_plugin import ToolProvider
 from dify_plugin.errors.tool import ToolProviderCredentialValidationError
 
-
 class ChartsProvider(ToolProvider):
     def _validate_credentials(self, credentials: dict[str, Any]) -> None:
         try:
