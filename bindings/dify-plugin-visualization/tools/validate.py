0d77a5cc9c6b0b6dc768acd3d57ed21de274a4b6
duxinyue.dxy <duxinyue.dxy@antgroup.com>
2025-05-28 16:58:04 +0800

    feat: valid params optimize

--- bindings/dify-plugin-visualization/tools/validate.py
+++ bindings/dify-plugin-visualization/tools/validate.py
@@ -5,17 +5,9 @@ from jsonschema import validate, ValidationError
 from typing import Any, Dict
 
 def validate_params(file_name: str, options: Dict[str, Any]):
-    try:
-        current_dir = os.path.dirname(__file__)
-        json_file_path = os.path.join(current_dir, 'charts-schema', f"{file_name}.json")
-        if not os.path.exists(json_file_path):
-            raise FileNotFoundError(f"File not found: {json_file_path}")
 
-        with open(json_file_path, 'r', encoding='utf-8') as file:
-            schema = json.load(file)
-
-        validate(instance=options, schema=schema)
-    except FileNotFoundError as e:
-        raise FileNotFoundError(f"File not found: {json_file_path}")
-    except ValidationError as e:
-        raise ValueError(f"Invalid data: {str(e)}")
+    current_dir = os.path.dirname(__file__)
+    json_file_path = os.path.join(current_dir, 'charts-schema', f"{file_name}.json")
+    with open(json_file_path, 'r', encoding='utf-8') as file:
+        schema = json.load(file)
+    validate(instance=options, schema=schema.get('inputSchema'))
