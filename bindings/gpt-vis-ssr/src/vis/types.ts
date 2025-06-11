0485985d4f67db802cdd0041006c0a283adf61e3
duxinyue.dxy <duxinyue.dxy@antgroup.com>
2025-06-11 14:31:34 +0800

    feat: add funnel chart

--- bindings/gpt-vis-ssr/src/vis/types.ts
+++ bindings/gpt-vis-ssr/src/vis/types.ts
@@ -2,4 +2,11 @@ export type CommonOptions = {
   width?: number;
   height?: number;
   theme?: string;
+  title?: string;
 };
+export interface BasePlotProps<T> {
+  data: T[];
+  axisXTitle?: string;
+  axisYTitle?: string;
+  legendTypeList?: string[];
+}
