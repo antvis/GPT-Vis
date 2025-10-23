dee9d7ddeee35ac732beea3afd08985e8d8e77da
duxinyue.dxy <duxinyue.dxy@antgroup.com>
2025-10-23 14:19:22 +0800

    feat: add test coverage

--- bindings/gpt-vis-ssr/jest.config.ts
+++ bindings/gpt-vis-ssr/jest.config.ts
@@ -4,6 +4,16 @@ const config: Config = {
   testTimeout: 60000,
   preset: 'ts-jest',
   collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html', 'json-summary'],
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+      tsconfig: {
+        isolatedModules: true,
+      },
+    },
+  },
   testRegex: '(/__tests__/.*\\.(test|spec))\\.ts$',
   collectCoverageFrom: ['src/**/*.ts'],
   setupFilesAfterEnv: ['./jest.setup.js'],
