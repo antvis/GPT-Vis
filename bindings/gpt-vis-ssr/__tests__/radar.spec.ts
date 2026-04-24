f2fa679ee168feae7570f1b89b25fffda8458c1b
q32757468 <43193877+q32757468@users.noreply.github.com>
2026-04-24 17:21:48 +0800

    Feat radar ssr align (#395)

--- bindings/gpt-vis-ssr/__tests__/radar.spec.ts
+++ bindings/gpt-vis-ssr/__tests__/radar.spec.ts
@@ -150,6 +150,30 @@ describe('SSR render', () => {
     vis.destroy();
   });
 
+  it('radar-align', async () => {
+    const vis = await render({
+      width: 600,
+      height: 400,
+      type: 'radar',
+      align: true,
+      data: [
+        { name: 'Design', group: 'a', value: 70 },
+        { name: 'Design', group: 'b', value: 30 },
+        { name: 'Development', group: 'a', value: 60 },
+        { name: 'Development', group: 'b', value: 70 },
+        { name: 'Marketing', group: 'a', value: 50 },
+        { name: 'Marketing', group: 'b', value: 60 },
+        { name: 'Users', group: 'a', value: 40 },
+        { name: 'Users', group: 'b', value: 50 },
+        { name: 'Test', group: 'a', value: 60 },
+        { name: 'Test', group: 'b', value: 70 },
+      ],
+    });
+
+    expect(vis.toBuffer()).toImageEqual('__tests__/snapshot', 'radar-align');
+    vis.destroy();
+  });
+
   it('radar-custom-style', async () => {
     const vis = await render({
       width: 600,
